03e9ffc531904109315eb2f61fa31d2fdd8c3ddc
liuhuanjie <2311383195@qq.com>
2023-04-27 18:07:23 +0800

    提交config,需要移动到系统盘根目录

---
+++ src/store/pathAll/index.js
@@ -0,0 +1,14 @@
+export default {
+  namespace: true,
+  state: {
+    pathAll: {}
+  },
+  getters: {},
+  mutations: {
+    changePathAll (state, payload) {
+      state.pathAll = payload
+    }
+  },
+  actions: {},
+  modules: {}
+}
